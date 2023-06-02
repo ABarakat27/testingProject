af0efdc4ef4921e89d2939d786161d6b740d35e6
Ahmedkhaledd1 <ahmedkhaledno1@gmail.com>
2023-06-02 19:50:51 +0300

    added Exceptions

--- src/testFiles/AccountTransferTest.java
+++ src/testFiles/AccountTransferTest.java
@@ -12,38 +12,35 @@ import static org.junit.Assert.assertEquals;
 
 public class AccountTransferTest {
 
-    static Account x ;
+    static Account x;
 
     @BeforeClass
-    public static void start(){
+    public static void start() {
 
-        x=new Account(4000);
+        x = new Account(4000);
         System.out.println("start testing");
     }
 
     @Test
     public void transferTest() throws InsufficientBalanceException {
-        Account x=new Account(1000);
-        Account y=new Account(5000);
-        y.transfer(2000,x);
-        assertEquals(3000,x.getBalance(),0.1);
-
+        Account x = new Account(1000);
+        Account y = new Account(5000);
+        y.transfer(2000, x);
+        assertEquals(3000, x.getBalance(), 0.1);
 
 
     }
+
     @Test
     public void InsufficientBalance() throws InsufficientBalanceException {
 
         try {
             new Account(0).transfer(1000, new Account(1000));
-        }
-        catch (InsufficientBalanceException e){
+        } catch (InsufficientBalanceException e) {
             System.out.println("handle it");
         }
 
     }
-
-
 
 
 }
