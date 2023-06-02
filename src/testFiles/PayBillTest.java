367560281ec471a24fc639bca783e10f5655c41b
Ahmedkhaledd1 <ahmedkhaledno1@gmail.com>
2023-06-02 18:45:03 +0300

    added test classes

--- src/testFiles/PayBillTest.java
+++ src/testFiles/PayBillTest.java
@@ -1,16 +1,34 @@
 package testFiles;
 
 import Implementation.Account;
+import Implementation.InsufficientBalanceException;
+import Implementation.PaidException;
+import Implementation.PayBill;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PayBillTest {
     static Account x ;
+    static PayBill pb;
 
     @BeforeClass
     public static void start(){
 
         x=new Account(4000);
+        pb=new PayBill(500.0,"electricity");
         System.out.println("start testing");
     }
+    @Test
+    public void PayBillTest() throws InsufficientBalanceException, PaidException {
+        try {
+            x.payBill(pb);
+            assertEquals(1500.0, x.getBalance(), 0.1);
+        }
+        catch (InsufficientBalanceException e){
+            System.out.println("handle it");
 
+        }
+    }
 }
