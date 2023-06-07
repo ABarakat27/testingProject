1a074c71b35a210176aaa44c250d978af9c2a87f
xYoussefMahmoudx <youssef.mahmoud.eng@gmail.com>
2023-06-08 00:13:19 +0300

    .

---
+++ src/GUI/TransferController.java
@@ -0,0 +1,25 @@
+package GUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TransferController {
+    @FXML
+    private TextField accNoText;
+
+    @FXML
+    private TextField amountText;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void transferFunc(ActionEvent event) {
+
+        SystemManger sys =System
+
+    }
+}
